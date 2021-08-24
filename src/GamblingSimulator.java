fb5f57f1ffbb0386d12e3a47e74bc35bc92a8f48
Vinay288 <vinayhiremath288@gmail.com>
2021-08-24 12:34:21 +0530

    ADDED:Calculating windays and loss days per month

--- src/GamblingSimulator.java
+++ src/GamblingSimulator.java
@@ -3,18 +3,24 @@ public class GamblingSimulator {
 	static final int GAMBLER_STAKE_EVERYDAY = 100;
 	static final int GAMBLER_BET_FOR_EACH_GAME = 1;
 	static final int GAMBLER_RESIGN_STAKE = (GAMBLER_STAKE_EVERYDAY * 50) / 100;
+	static final int TOTAL_DAYS = 20;
+	static final int MONTH = 30;
 
 	public static void main(String[] args) {
-		resignStakeFor20Days();
+
+		gambleForOneYear();
+
 	}
 
 	static int gamblerGame() {
+
 		int outcomeOfGame = (int) (Math.random() * 2);
 		return outcomeOfGame;
 
 	}
 
 	static void resignStakePerDay() {
+
 		int totalAmountForDay = GAMBLER_STAKE_EVERYDAY;
 		while (totalAmountForDay != GAMBLER_RESIGN_STAKE + GAMBLER_STAKE_EVERYDAY
 				&& totalAmountForDay != GAMBLER_STAKE_EVERYDAY - GAMBLER_RESIGN_STAKE) {
@@ -29,13 +35,17 @@ public class GamblingSimulator {
 	}
 
 	static void resignStakeFor20Days() {
+
 		int totalAmount = GAMBLER_STAKE_EVERYDAY;
 		int totalLoss = 0, totalWin = 0;
-		for (int i = 0; i < 20; i++) {
+
+		for (int i = 0; i < TOTAL_DAYS; i++) {
+
 			int totalAmountForDay = GAMBLER_STAKE_EVERYDAY;
 			while (totalAmountForDay != GAMBLER_RESIGN_STAKE + GAMBLER_STAKE_EVERYDAY
 					&& totalAmountForDay != GAMBLER_STAKE_EVERYDAY - GAMBLER_RESIGN_STAKE) {
 				int outcome = gamblerGame();
+
 				if (outcome == 1) {
 					totalAmountForDay++;
 				} else {
@@ -49,8 +59,44 @@ public class GamblingSimulator {
 			totalAmount += totalAmountForDay;
 			System.out.println("Resigning for the day with money " + totalAmountForDay);
 		}
-		System.out.println("Total win at the end of 20 days" + totalWin);
-		System.out.println("Total loose at the end of 20 days" + totalLoss);
-		System.out.println("Total amount at the end of 20 days" + totalAmount);
+
+		System.out.println("Total win at the end of 20 days " + totalWin);
+		System.out.println("Total loose at the end of 20 days " + totalLoss);
+		System.out.println("Total amount at the end of 20 days " + totalAmount);
+	}
+
+	static void gambleForOneYear() {
+		int totalAmount = 0;
+		for (int j = 0; j < 12; j++) {
+			int totalLoss = 0, totalWin = 0, totalWinDays = 0, totalLossDays = 0;
+			for (int i = 0; i < MONTH; i++) {
+
+				int totalAmountForDay = GAMBLER_STAKE_EVERYDAY;
+				while (totalAmountForDay != GAMBLER_RESIGN_STAKE + GAMBLER_STAKE_EVERYDAY
+						&& totalAmountForDay != GAMBLER_STAKE_EVERYDAY - GAMBLER_RESIGN_STAKE) {
+					int outcome = gamblerGame();
+
+					if (outcome == 1) {
+						totalAmountForDay++;
+					} else {
+						totalAmountForDay--;
+					}
+				}
+				if (totalAmountForDay < 100) {
+					totalLoss += GAMBLER_RESIGN_STAKE;
+					totalLossDays++;
+				} else {
+					totalWin += GAMBLER_RESIGN_STAKE;
+					totalWinDays++;
+				}
+				totalAmount += totalAmountForDay;
+			}
+			System.out.println("month*********  " + (j+1));
+			System.out.println("Total number of days won in one month  " + totalWinDays );
+			System.out.println("Total number of days lost in one month  " + totalLossDays);
+			System.out.println("Total win at the end month " + totalWin);
+			System.out.println("Total loose at the month " + totalLoss);
+			System.out.println("Total amount at the month " + totalAmount);
+		}
 	}
 }

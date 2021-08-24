b6006d8135daa7bddd44992c655f07932b8cd4f5
Vinay288 <vinayhiremath288@gmail.com>
2021-08-24 09:10:09 +0530

    Added: feature to resign for the day if wins or looses 50% of the stake

--- src/GamblingSimulator.java
+++ src/GamblingSimulator.java
@@ -2,18 +2,19 @@
 public class GamblingSimulator {
 	static final int GAMBLER_STAKE_EVERYDAY = 100;
 	static final int GAMBLER_BET_FOR_EACH_GAME = 1;
+	static final int GAMBLER_50_PERCENT_STAKE=(GAMBLER_STAKE_EVERYDAY*50)/100;
 
 	public static void main(String[] args) {
 		int totalAmountForDay = GAMBLER_STAKE_EVERYDAY;
+		while(totalAmountForDay!=GAMBLER_50_PERCENT_STAKE+GAMBLER_STAKE_EVERYDAY && totalAmountForDay!=GAMBLER_STAKE_EVERYDAY-GAMBLER_50_PERCENT_STAKE) {
 		int outcome = gamblerGame();
-
 		if (outcome == 1) {
 			totalAmountForDay++;
-			System.out.println("win");
 		} else {
 			totalAmountForDay--;
-			System.out.println("loose");
 		}
+		}
+		System.out.println("Resigning for the day with money "+totalAmountForDay);
 	}
 
 	static int gamblerGame() {

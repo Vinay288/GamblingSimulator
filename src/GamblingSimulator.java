51ced05e3a5d10c55ba1919464503fb17e7fc6a8
Vinay288 <vinayhiremath288@gmail.com>
2021-08-24 08:50:33 +0530

    Refactor: added win or loose functionality

--- src/GamblingSimulator.java
+++ src/GamblingSimulator.java
@@ -3,7 +3,17 @@ public class GamblingSimulator {
 	static final int GAMBLER_STAKE_EVERYDAY = 100;
 	static final int GAMBLER_BET_FOR_EACH_GAME = 1 ;
 	public static void main(String[] args) {
+		int outcome = gamblerGame();
 		
+		if(outcome==1)
+			System.out.println("win");
+		else
+			System.out.println("loose");
+	}
+	static int gamblerGame() {
+		int outcomeOfGame=(int)(Math.random()*2);
+		System.out.println(outcomeOfGame);
+		return outcomeOfGame;
 		
 	}
 }

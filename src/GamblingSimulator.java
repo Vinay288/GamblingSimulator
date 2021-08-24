eb8d3524d3d1553189406da19d98a3c1fb575a81
Vinay288 <vinayhiremath288@gmail.com>
2021-08-24 08:57:50 +0530

    refactor : increment amount on win and decremnt on loose

--- src/GamblingSimulator.java
+++ src/GamblingSimulator.java
@@ -1,19 +1,24 @@
 
 public class GamblingSimulator {
 	static final int GAMBLER_STAKE_EVERYDAY = 100;
-	static final int GAMBLER_BET_FOR_EACH_GAME = 1 ;
+	static final int GAMBLER_BET_FOR_EACH_GAME = 1;
+
 	public static void main(String[] args) {
+		int totalAmountForDay = GAMBLER_STAKE_EVERYDAY;
 		int outcome = gamblerGame();
-		
-		if(outcome==1)
+
+		if (outcome == 1) {
+			totalAmountForDay++;
 			System.out.println("win");
-		else
+		} else {
+			totalAmountForDay--;
 			System.out.println("loose");
+		}
 	}
+
 	static int gamblerGame() {
-		int outcomeOfGame=(int)(Math.random()*2);
-		System.out.println(outcomeOfGame);
+		int outcomeOfGame = (int) (Math.random() * 2);
 		return outcomeOfGame;
-		
+
 	}
 }

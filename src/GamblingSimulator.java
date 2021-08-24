1d26e6f42a643cdfd669a8e86629c52f9690adaf
Vinay288 <vinayhiremath288@gmail.com>
2021-08-24 08:30:53 +0530

    Refactored: initialized stake and stake for each game constants

--- src/GamblingSimulator.java
+++ src/GamblingSimulator.java
@@ -1,7 +1,9 @@
 
 public class GamblingSimulator {
+	static final int GAMBLER_STAKE_EVERYDAY = 100;
+	static final int GAMBLER_BET_FOR_EACH_GAME = 1 ;
 	public static void main(String[] args) {
-		final int GAMBLER_STAKE_EVERYDAY = 100;
-		final int GAMBLER_BET_FOR_EACH_GAME = 1;
+		
+		
 	}
 }
